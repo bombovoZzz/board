07a00995b0310a8b0878d63760bd09d809561a7c
igoryan112 <kozurev_igor@mail.ru>
2018-10-20 18:19:35 +0300

    change ads js

--- frontend/web/js/header.js
+++ frontend/web/js/header.js
@@ -1,20 +1,5 @@
 $(document).ready(function () {
 	
-	// автоопределение высоты блока header	
-	$(function () {
-		var header = $('#header-js'),
-		    heightWrap = header.children().height();
-
-		header.css('height', heightWrap);
-
-		$(window).resize(function () {
-
-			heightWrap = header.children().height();
-			header.css('height', heightWrap);
-
-		});
-
-	});
 
 
 });
